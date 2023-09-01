c66302961b623abb4f3702ac8acb34c0925e0a83
Salmah Lasisi <44398948+salmah52@users.noreply.github.com>
2023-09-01 19:27:26 +0100

    Update ProcessS3Files..py

--- ProcessS3Files..py
+++ ProcessS3Files..py
@@ -13,12 +13,11 @@ def lambda_handler(event, context):
         # Process the file (replace with your processing logic)
         try:
             # Your processing logic goes here
-            # For example, you can read the file content or perform transformations
-            # For simplicity, let's assume you're printing the object key
+           
             processed_data = f"Processing file in bucket: {s3_bucket}, key: {s3_object_key}"
             print(processed_data)
 
-            # Publish a message to an SNS topic (replace 'YourSNSTopicArn' with your SNS topic ARN)
+            # Publish a message to an SNS topic 
             sns_client.publish(
                 TopicArn='arn:aws:sns:us-east-1:693939771894:FileProcessingTopic',
                 Message=processed_data
@@ -27,4 +26,4 @@ def lambda_handler(event, context):
             # Handle any errors that may occur during processing
             print(f"Error processing file in bucket: {s3_bucket}, key: {s3_object_key}: {str(e)}")
 
-# Your Lambda function will continue running and processing events as they occur.
+
